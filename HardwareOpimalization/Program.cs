3c26c932856db8ea48b376f8d4a5dbe62bfcec91
MG174 <goryczko99@gmail.com>
2023-03-17 23:04:29 +0100

    made parallel and added mock data possibility

--- HardwareOpimalization/Program.cs
+++ HardwareOpimalization/Program.cs
@@ -1,4 +1,6 @@
-﻿public class Item 
+﻿using System.Collections.Concurrent;
+
+public class Item
 {
     public string name { get; set; }
     public int X { get; set; }
@@ -24,60 +26,95 @@ class Program
         List<Item> D = new List<Item>();
         List<Item> E = new List<Item>();
 
-        A.Add(new Item { name = "a1", X = 5000, Y = 1000 });
-        A.Add(new Item { name = "a2", X = 2000, Y = 1000 });
+        //Real data 
+        //A.Add(new Item { name = "a1", X = 5000, Y = 1000 });
+        //A.Add(new Item { name = "a2", X = 2000, Y = 1000 });
+        //B.Add(new Item { name = "b1", X = 1400, Y = 1000 });
+        //B.Add(new Item { name = "b2", X = 2000, Y = 900 });
+        //C.Add(new Item { name = "c1", X = 1300, Y = 1000 });
+        //C.Add(new Item { name = "c2", X = 2000, Y = 900 });
+        //D.Add(new Item { name = "d1", X = 6000, Y = 1000 });
+        //D.Add(new Item { name = "d2", X = 2000, Y = 1500 });
+        //E.Add(new Item { name = "e1", X = 1100, Y = 1000 });
+        //E.Add(new Item { name = "e2", X = 2000, Y = 2000 });
 
-        B.Add(new Item { name = "b1", X = 1400, Y = 1000 });
-        B.Add(new Item { name = "b2", X = 2000, Y = 900 });
+        //Random mock data
+        for (int i = 0; i < 1000000; i++) 
+        {
+            A.Add(new Item { name = $"{RandomGenerator.RandomString(10)}", X = RandomGenerator.RandomInt(500,5000), Y = RandomGenerator.RandomInt(500, 5000) });
+            B.Add(new Item { name = $"{RandomGenerator.RandomString(10)}", X = RandomGenerator.RandomInt(500, 5000), Y = RandomGenerator.RandomInt(500, 5000) });
+            C.Add(new Item { name = $"{RandomGenerator.RandomString(10)}", X = RandomGenerator.RandomInt(500, 5000), Y = RandomGenerator.RandomInt(500, 5000) });
+            D.Add(new Item { name = $"{RandomGenerator.RandomString(10)}", X = RandomGenerator.RandomInt(500, 5000), Y = RandomGenerator.RandomInt(500, 5000) });
+            E.Add(new Item { name = $"{RandomGenerator.RandomString(10)}", X = RandomGenerator.RandomInt(500, 5000), Y = RandomGenerator.RandomInt(500, 5000) });
+        }
 
-        C.Add(new Item { name = "c1", X = 1300, Y = 1000 });
-        C.Add(new Item { name = "c2", X = 2000, Y = 900 });
-
-        D.Add(new Item { name = "d1", X = 6000, Y = 1000 });
-        D.Add(new Item { name = "d2", X = 2000, Y = 1500 });
-
-        E.Add(new Item { name = "e1", X = 1100, Y = 1000 });
-        E.Add(new Item { name = "e2", X = 2000, Y = 2000 });
+        A = A.OrderByDescending(x => x.X / x.Y).ToList();
+        B = B.OrderByDescending(x => x.X / x.Y).ToList();
+        C = C.OrderByDescending(x => x.X / x.Y).ToList();
+        D = D.OrderByDescending(x => x.X / x.Y).ToList();
+        E = E.OrderByDescending(x => x.X / x.Y).ToList();
 
         var minY = 4500;
         var maxY = 5500;
 
-        List<PackOfItems> solutions = new List<PackOfItems>();
+        ConcurrentQueue<PackOfItems> solutions = new ConcurrentQueue<PackOfItems>();
 
         var maxX = 0;
 
-        foreach (var itemA in A)
+        var interation = 0;
+
+        Parallel.ForEach(A, itemA =>
         {
-            foreach (var itemB in B)
+            Parallel.ForEach(B, itemB =>
             {
-                foreach (var itemC in C)
+                Parallel.ForEach(C, itemC =>
                 {
-                    foreach (var itemD in D)
+                    Parallel.ForEach(D, itemD =>
                     {
-                        foreach (var itemE in E)
+                        Parallel.ForEach(E, itemE =>
                         {
                             var totalX = itemA.X + itemB.X + itemC.X + itemD.X + itemE.X;
                             var totalY = itemA.Y + itemB.Y + itemC.Y + itemD.Y + itemE.Y;
+                            interation++;
+                            //Console.WriteLine("Iteration " + interation);
 
-                            if ((minY <= totalY) && (totalY <= maxY)) 
+                            if ((minY <= totalY) && (totalY <= maxY))
                             {
                                 if (totalX > maxX)
                                 {
                                     maxX = totalX;
                                     solutions.Clear();
-                                    solutions.Add(new PackOfItems { A = itemA, B = itemB, C = itemC, D = itemD, E = itemE, maxValue = maxX });
-                                } else if (totalX == maxX) 
+                                    solutions.Append(new PackOfItems { A = itemA, B = itemB, C = itemC, D = itemD, E = itemE, maxValue = maxX });
+                                    Console.WriteLine("Valid solution: Y: " + totalY + ", X: " + totalX);
+                                }
+                                else if (totalX == maxX)
                                 {
-                                    solutions.Add(new PackOfItems { A = itemA, B = itemB, C = itemC, D = itemD, E = itemE, maxValue = maxX });
+                                    solutions.Append(new PackOfItems { A = itemA, B = itemB, C = itemC, D = itemD, E = itemE, maxValue = maxX });
+                                    Console.WriteLine("Valid solution: Y: " + totalY + ", X: " + totalX);
                                 }
                             }
-                        }
-                    }
-                }
-            }
-        }
+                        });
+                    });
+                });
+            });
+        });
 
         var best = solutions.MaxBy(x => x.maxValue);
+    }
+}
 
-    } 
+public static class RandomGenerator
+{
+    public static string RandomString(int length)
+    {
+        Random random = new Random();
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        return new string(Enumerable.Repeat(chars, length)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
+    }
+    public static int RandomInt(int min, int max)
+    {
+        Random random = new Random();
+        return random.Next(min, max);
+    }
 }
